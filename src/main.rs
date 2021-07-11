fea94109bbb08b2a6628485752523bdde980df3d
Halvor Fladsrud Bo <halvor@complex.codes>
2021-07-11 02:15:09 +0100

    Made keyboard input work

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use pc_keyboard::{layouts, HandleControl, KeyState, Keyboard, ScancodeSet2};
+use pc_keyboard::{layouts, HandleControl, KeyState, Keyboard, ScancodeSet1};
+use uart_16550::SerialPort;
 use x86_64::instructions::port::Port;
 
 static SERIAL_IO_PORT: u16 = 0xf4;
@@ -10,16 +11,52 @@ static SERIAL_IO_PORT: u16 = 0xf4;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
-    let mut keyboard = Port::<u32>::new(0x60);
+    let mut keyboard_port = unsafe { SerialPort::new(0x60) };
+    keyboard_port.init();
+    let mut keyboard = Keyboard::new(
+        layouts::Us104Key,
+        ScancodeSet1,
+        HandleControl::MapLettersToUnicode,
+    );
+
+    let mut pointer = 0;
+    let mut prev = 0;
+
     loop {
         unsafe {
-            let data = read(&mut keyboard);
-            display(vga_buffer, data, 2, 100);
-            display(vga_buffer, data >> 24, 2, 0);
-            display(vga_buffer, data >> 24, 10, 40);
-            display(vga_buffer, (data >> 16) % 256, 2, 9);
-            display(vga_buffer, (data >> 8) % 256, 2, 18);
-            display(vga_buffer, data % 256, 2, 27);
+            let data = keyboard_port.receive();
+            match keyboard.add_byte(data) {
+                Ok(Some(event)) => {
+                    let code = event.code as u8;
+                    match event.state {
+                        KeyState::Up => {
+                            prev = 0;
+                            continue;
+                        }
+                        KeyState::Down => {}
+                    }
+                    if code < 78 || prev == code {
+                        continue;
+                    }
+                    write(vga_buffer, pointer, code - 78 + 97);
+                    pointer += 1;
+                    prev = code;
+                }
+                Ok(None) => {
+                    continue;
+                }
+                Err(e) => {
+                    write(vga_buffer, 40, 42);
+                }
+            }
+
+            //display(vga_buffer, data, 2, 100);
+            //display(vga_buffer, data >> 24, 2, 0);
+            //display(vga_buffer, data >> 24, 10, 40);
+            //display(vga_buffer, (data >> 16) % 256, 2, 9);
+            //display(vga_buffer, (data >> 8) % 256, 2, 18);
+            // display(vga_buffer, data, 2, 0);
+            // display(vga_buffer, data, 10, 10);
         }
     }
     unsafe {
